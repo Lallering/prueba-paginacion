450e90805620791b9553cea477ce9a33d14cd6d8
Erick Castañeda <neoris.alberto@coppel.com>
2023-12-08 12:45:15 -0600

    Initial Commit

---
+++ Prueba Paginacion/Prueba_PaginacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_PaginacionApp.swift
+//  Prueba Paginacion
+//
+//  Created by erick.castaneda on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_PaginacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
